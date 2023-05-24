955e8dd5c8f40f2a90d4b6654d490046cf95dd99
ErickErickErickErick <erick_braatz@estudante.sesisenai.org.br>
2023-05-24 07:50:00 -0300

    first commit

---
+++ CAIXA_PEQUENA.js
@@ -0,0 +1,20 @@
+function replaceName(image)
+{
+    namejs.src=image;
+}
+function replaceCart(image)
+{
+    cart.src=image;
+}
+function replaceProfile(image)
+{
+    profile.src=image;
+}
+function replaceBox(image)
+{
+    box.src=image;
+}
+function replaceBack(image)
+{
+    seta.src=image;
+}
